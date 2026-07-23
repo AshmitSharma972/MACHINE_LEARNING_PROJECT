6f4e9b818df3e3210896beff86349766af993c38
Ashmit Sharma <sashmit972@gmail.com>
2026-07-23 12:49:02 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+    name='MACHINE_LEARNING_PROJECT'
+    version='0.0.1'
+    author='Ashmit Sharma'
+    author_email='sashmit972@gmail.com'
+    packages=find_packages()
+    install_requires=['pandas','numpy','seaborn']
+)
