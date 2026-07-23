645c509442e13adabcf31c9fca1ffe441a0abbbb
Ashmit Sharma <sashmit972@gmail.com>
2026-07-23 12:53:32 +0530

    setup_update

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
+from typing import List
 
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+
+    return requirements
 
 
 setup(
-    name='MACHINE_LEARNING_PROJECT'
-    version='0.0.1'
-    author='Ashmit Sharma'
-    author_email='sashmit972@gmail.com'
-    packages=find_packages()
-    install_requires=['pandas','numpy','seaborn']
+    name="MACHINE_LEARNING_PROJECT",
+    version="0.0.1",
+    author="Ashmit Sharma",
+    author_email="sashmit972@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
 )
