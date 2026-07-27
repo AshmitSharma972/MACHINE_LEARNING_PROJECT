c24b1f95f57adb361b7d04214568591c78c01a47
Ashmit Sharma <warloard2005@gmail.com>
2026-07-27 14:05:10 +0530

    modification of the file not solved error

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,53 +1,96 @@
 import os
 import sys
+from dataclasses import dataclass
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
 from src.exception import CustomException
 from src.logger import logging
-import pandas as pd
+from src.components.data_transfomation import DataTransformer
+# If you renamed the file, use:
+# from src.components.data_transformation import DataTransformer
 
-from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path:str=os.path.join('artifact',"train.csv")
-    test_data_path:str=os.path.join('artifact',"test.csv")
-    raw_data_path:str=os.path.join('artifact',"data.csv")
+    train_data_path: str = os.path.join("artifact", "train.csv")
+    test_data_path: str = os.path.join("artifact", "test.csv")
+    raw_data_path: str = os.path.join("artifact", "data.csv")
+
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_congif = DataIngestionConfig()
+        self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info("entered the data ingestion method")
+        logging.info("Entered the data ingestion method")
 
         try:
-            df = pd.read_csv('notebook\data\StudentsPerformance.csv')
-            logging.info('read the dataset as dataframe')
+            # Read dataset
+            df = pd.read_csv(r"notebook\data\StudentsPerformance.csv")
+            logging.info("Read the dataset as DataFrame")
 
-
-            os.makedirs(os.path.dirname(self.ingestion_congif.train_data_path),exist_ok=True)
-
-
-            df.to_csv(self.ingestion_congif.raw_data_path,index=False,header=True)
-
-            logging.info("train test split")
-            train_set,test_set = train_test_split(df,test_size=0.25,random_state=42)
-
-            train_set.to_csv(self.ingestion_congif.train_data_path,index=False,header=True)
-
-            test_set.to_csv(self.ingestion_congif.test_data_path,index=False,header=True)
-
-            logging.info("ingestion of the data is completed")
-
-            return(
-                self.ingestion_congif.train_data_path,
-                self.ingestion_congif.test_data_path
-
-
+            # Create artifact directory
+            os.makedirs(
+                os.path.dirname(self.ingestion_config.train_data_path),
+                exist_ok=True
             )
-        except Exception as e:
-            raise CustomException(e,sys)
 
-if __name__=="__main__":
+            # Save raw data
+            df.to_csv(
+                self.ingestion_config.raw_data_path,
+                index=False,
+                header=True
+            )
+
+            logging.info("Train-Test Split Initiated")
+
+            train_set, test_set = train_test_split(
+                df,
+                test_size=0.25,
+                random_state=42
+            )
+
+            train_set.to_csv(
+                self.ingestion_config.train_data_path,
+                index=False,
+                header=True
+            )
+
+            test_set.to_csv(
+                self.ingestion_config.test_data_path,
+                index=False,
+                header=True
+            )
+
+            logging.info("Data Ingestion Completed")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+if __name__ == "__main__":
+
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformer()
+
+    train_arr, test_arr, preprocessor_path = (
+        data_transformation.initiate_data_transformation(
+            train_data,
+            test_data
+        )
+    )
+
+    print("Data Transformation Completed Successfully")
+    print("Train Array Shape :", train_arr.shape)
+    print("Test Array Shape  :", test_arr.shape)
+    print("Preprocessor Path :", preprocessor_path)
